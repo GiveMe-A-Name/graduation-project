c15cc996f5ee1bf69bda29dc70bfe69c6f17e6d7
GiveMe-A-Name <a455300764b@hotmail.com>
2022-04-04 10:56:36 +0800

    refactor: register

--- FE/src/router/index.ts
+++ FE/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-import Home from '@/views/Home/index.vue';
-import Register from '@/views/Register/index.vue';
+import Home from '@/views/home/index.vue';
+import Register from '@/views/register/index.vue';
 
 const routes: RouteRecordRaw[] = [
   { component: Home, path: '/' },
